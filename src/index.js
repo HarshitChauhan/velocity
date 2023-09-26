12ba3e59d0ec71047c03c7722aeb9ffa31b5db4f
Harshit Kumar <harshitkumar2503@gmail.com>
2023-09-26 23:00:11 +0530

    syncing blockchains

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const request = require("request");
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
 const blockchain = new Blockchain();
 
-const PORT = 3001;
+const DEFAULT_PORT = 3001;
+let PEER_PORT;
+if (process.env.GENERATE_PEER_PORT === 'true') PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
 const app = express();
-const pubsub = new PubSub({blockchain});
-setTimeout(()=> pubsub.broadcastChain(),1000);
+const pubsub = new PubSub({ blockchain });
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 // for( let i = 0; i< 10;i++){
@@ -22,10 +29,25 @@ app.get('/api/blocks', (req, res) => {
 app.post('/api/blocks', (req, res) => {
     const { data } = req.body;
     blockchain.addBlock({ data });
+    pubsub.broadcastChain();
     res.redirect('/api/blocks');
-    // res.send(blockchain.chain);
-})
+});
 
-app.listen(PORT, () =>
-    console.log(`Server running on port ${PORT}`)
-);
+// to sync blockchains from different nodes
+const syncBlockchains = () => {
+    request(
+        { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+        (error, reposnse, body) => {
+            if (!error && reposnse.statusCode === 200) {
+                const rootChain = JSON.parse(body);
+                console.log("Replace chain on sync with", rootChain);
+                blockchain.replaceChain(rootChain);
+            }
+        }
+    );
+};
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    syncBlockchains();
+});
